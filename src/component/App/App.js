49fec45b4a739183ea7cf802cb58218e0ad802cd
Hliana <liana.niehrah@gmail.com>
2019-06-12 18:44:46 -0700

    h1

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -4,11 +4,15 @@ import './App.scss';
 import Footer from '../Footer/Footer';
 import Navigation from "../Navigation/Navigation";
 
+import {Box} from "@material-ui/core";
+
 function App() {
   return (
     <main>
       <div id="header">
-        {/*<h1>Create memories, together.</h1>*/}
+        <Box>
+          <h1>Create memories, together.</h1>
+        </Box>
       </div>/>
       <Navigation/>
       <Footer/>
