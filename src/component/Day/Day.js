439c9e674970fb3bc670e92cd4f53cb61223239b
Rick Bell <richard.bellamy123@gmail.com>
2019-06-18 20:59:42 -0700

    itinerary work comming along

--- src/component/Day/Day.js
+++ src/component/Day/Day.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Paper } from "@material-ui/core";
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 export default class Day extends React.Component{
     render(){
         return(
-            <div id="dayDiv">
-                <div id="dayHeader">
-                    <h6>{this.props.date}</h6>
-                </div>
-                <div id="dayBody">
-                    <ul>
-                        {this.props.hours.map(hour =>
-                            <li>
-                                {hour.hourTime} : {hour.filled ? hour.eventName : 'unfilled'}
-                            </li>
-                        )}
-                    </ul>
-                </div>
+            <div>
+                <Paper className="dayPaper">
+                    <Typography component="h5">
+                      {this.props.date}
+                    </Typography>
+                    <List component="ul" className="hoursList">
+                      {this.props.hours.map(hour=>
+                        <ListItem component="li">
+                            <ListItemText primary={`${hour.hourTime} : ${hour.eventName ? hour.eventName : 'Available'}`}/>
+                        </ListItem>
+                      )}
+                    </List>
+                </Paper>
             </div>
         );
     }
--- src/component/Itinerary/Itinerary.js
+++ src/component/Itinerary/Itinerary.js
@@ -1,90 +1,96 @@
-import React from 'react';
-import Day from '../Day/Day';
-import { connect } from 'react-redux';
-import Navigation from '../Navigation/Navigation';
+import React from "react";
+import Day from "../Day/Day";
+import { connect } from "react-redux";
+import Navigation from "../Navigation/Navigation";
 //import {object} from "prop-types";
+import './Itinerary.scss';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 
-class Itinerary extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            itineraryHeaderStyle: {
-                backgroundColor: '#008080',
-                // backgroundImage://img link here
-            },
-            testItinerary: {
-                itineraryName: 'test',
-                itineraryLocation: 'lab',
-                arrayOfDays: [
-                    {
-                     date: '4/20',
-                     hours:[
-                         {filled: false, eventName: null, hourTime:'9am'},
-                         {filled: true, eventName: 'breakfast', hourTime:'10am'},
-                         {filled: false, eventName: null, hourTime:'11am'},
-                         {filled: true, eventName: 'hike', hourTime:'12pm'},
-                         {filled: true, eventName: 'hike', hourTime:'1pm'},
-                         {filled: false, eventName: null, hourTime:'2pm'},
-                         {filled: true, eventName: 'dinner', hourTime:'3pm'},
-                         {filled: false, eventName: null, hourTime:'4pm'},
-                     ],
-                    },
-                    {
-                        date: '4/21',
-                        hours:[
-                            {filled: false, eventName: null, hourTime:'9am'},
-                            {filled: false, eventName: null, hourTime:'10am'},
-                            {filled: false, eventName: null, hourTime:'11am'},
-                            {filled: true, eventName: 'run', hourTime:'12am'},
-                            {filled: true, eventName: 'stretch', hourTime:'1pm'},
-                            {filled: true, eventName: 'snack', hourTime:'2pm'},
-                            {filled: true, eventName: 'yoga', hourTime:'3pm'},
-                            {filled: true, eventName: 'yoga', hourTime:'4pm'},
-                        ],
-                    },
-                ],
-            }
-        }
+
+class Itinerary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itineraryHeaderStyle: {
+        backgroundColor: "#008080"
+        // backgroundImage://img link here
+      },
+      testItinerary: {
+        itineraryName: "testItinerary",
+        itineraryLocation: "lab",
+        arrayOfDays: [
+          {
+            date: "4/20",
+            hours: [
+              { filled: false, eventName: null, hourTime: "9am" },
+              { filled: true, eventName: "breakfast", hourTime: "10am" },
+              { filled: false, eventName: null, hourTime: "11am" },
+              { filled: true, eventName: "hike", hourTime: "12pm" },
+              { filled: true, eventName: "hike", hourTime: "1pm" },
+              { filled: false, eventName: null, hourTime: "2pm" },
+              { filled: true, eventName: "dinner", hourTime: "3pm" },
+              { filled: false, eventName: null, hourTime: "4pm" }
+            ]
+          },
+          {
+            date: "4/21",
+            hours: [
+              { filled: false, eventName: null, hourTime: "9am" },
+              { filled: false, eventName: null, hourTime: "10am" },
+              { filled: false, eventName: null, hourTime: "11am" },
+              { filled: true, eventName: "run", hourTime: "12am" },
+              { filled: true, eventName: "stretch", hourTime: "1pm" },
+              { filled: true, eventName: "snack", hourTime: "2pm" },
+              { filled: true, eventName: "yoga", hourTime: "3pm" },
+              { filled: true, eventName: "yoga", hourTime: "4pm" }
+            ]
+          }
+        ]
+      }
     };
+  }
 
-    render(){
-        return(
-            <div>
-                <Navigation />
-                <div id="itineraryHeader" style={this.state.itineraryHeaderStyle}>
-                    <h1>{this.state.testItinerary.itineraryName}</h1>
-                    <h3>{this.state.testItinerary.itineraryLocation}</h3>
-                </div>
-                <div>
-                    {
-                        this.state.testItinerary.arrayOfDays.map(day =>
-                            <Day
-                                date={day.date}
-                                hours={day.hours}
-                            />
-                        )
-                        /*this.props.days.map(day => {
-                            <Day
-                                date={day.date}
-                                hours={day.hours}
-                            />
-                        })*/
-                    }
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <div id="itineraryHeader" style={this.state.itineraryHeaderStyle}>
+          <h1>{this.state.testItinerary.itineraryName}</h1>
+          <h3>{this.state.testItinerary.itineraryLocation}</h3>
+        </div>
+
+        <Grid
+          container
+          direction="row"
+          justify="space-evenly"
+          alignItems="center"
+          id="dayContainerDiv"
+        >
+          {this.state.testItinerary.arrayOfDays.map(day=>
+            <Grid item>
+              <Day className="paper"
+                   hours={day.hours}
+                   date={day.date}
+              />
+            </Grid>
+          )}
+        </Grid>
+      </div>
+    );
+  }
 }
 const mapStateToProps = state => {
-    return{
-        days: state,
-        //itineraryBackgroundImage: ,
-    }
+  return {
+    days: state
+    //itineraryBackgroundImage: ,
+  };
 };
 const mapDispatchToProps = dispatch => {
-    return{
-
-    }
+  return {};
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Itinerary);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Itinerary);
