7ca32291065b1cefe4704c44505a376423774219
Hliana <liana.niehrah@gmail.com>
2019-06-19 18:52:24 -0700

    itinerary form and reducers

---
+++ src/reducer/itinerary-reducer.js
@@ -0,0 +1,16 @@
+export default (state = {}, {type, payload}) => {
+	switch(type) {
+		case 'CREATE_ITINERARY':
+			return {...state, payload};
+		case 'EDIT_ITINERARY':
+			let newState = {...state};
+			newState[payload.id] = payload;
+			return newState;
+		case 'DELETE_ITINERARY':
+			let updatedState = {...state};
+			delete updatedState[payload.id];
+			return updatedState;
+		default:
+			return state;
+	}
+};
